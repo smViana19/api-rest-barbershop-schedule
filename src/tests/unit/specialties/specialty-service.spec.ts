3f65779605063e78815e8e7e85b6c9f541c0fd60
Samuu <samuelfilipevianadois@gmail.com>
2024-12-05 15:08:58 -0300

    test: specialties tested - create and find specialties - return errors with not found specialtyId

--- src/tests/unit/specialties/specialty-service.spec.ts
+++ src/tests/unit/specialties/specialty-service.spec.ts
@@ -8,7 +8,7 @@ describe('SpecialtyService', () => {
   beforeEach(() => {
     specialtyService = new SpecialtyService();
   });
-  describe('create', () => {
+  describe('createSpecialty', () => {
     it('should be create a new speciality', async () => {
       const specialtyMock = {
         name: "barba",
@@ -24,6 +24,55 @@ describe('SpecialtyService', () => {
         message: "Especialidade criada com sucesso"
       });
     });
-  })
+  });
+  describe('getAllSpecialties', () => {
+    it('should be get all specialities', async () => {
+      const specialtiesMock = [
+        { id: 1, name: 'barba1', description: 'especialidade1' },
+        { id: 2, name: 'barba2', description: 'especialidade2' },
+        { id: 3, name: 'barba3', description: 'especialidade3' },
+      ];
+      (Specialty.findAll as jest.Mock).mockResolvedValue(specialtiesMock);
+      const response = await specialtyService.getAllSpecialties();
+      expect(Specialty.findAll).toHaveBeenCalledTimes(1);
+      expect(response).toEqual({
+        status: 200,
+        message: specialtiesMock,
+      });
+    });
+  });
+
+  describe('getSpecialtyById', () => {
+    it('should be get specialty by id', async () => {
+      const specialtyIdMock = '1';
+      const specialtyMock = {
+        id: 1,
+        name: 'teste',
+        description: 'teste description',
+      };
+      (Specialty.findByPk as jest.Mock).mockResolvedValue(specialtyMock);
+      const response = await specialtyService.getSpecialtyById(specialtyIdMock);
+
+      expect(Specialty.findByPk).toHaveBeenCalledWith(specialtyIdMock);
+      expect(Specialty.findByPk).toHaveBeenCalledTimes(1);
+
+      expect(response).toEqual({
+        status: 200,
+        message: specialtyMock,
+      });
+    });
+
+    it('should be return error when specialty not found', async () => {
+      const specialtyIdMock = '1';
+      (Specialty.findByPk as jest.Mock).mockResolvedValue(null);
+      const response = await specialtyService.getSpecialtyById(specialtyIdMock);
+      expect(Specialty.findByPk).toHaveBeenCalledWith(specialtyIdMock)
+      expect(Specialty.findByPk).toHaveBeenCalledTimes(1);
+      expect(response).toEqual({
+        message: { message: 'Especialidade não encontrada' },
+        status: 404
+      });
+    });
+  });
 
 })
